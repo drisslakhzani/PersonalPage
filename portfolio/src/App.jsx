12a4525d46fcc27fcc009b9f8c41dba729089df3
drisslakhzani <driss.lakhzani.lkz@gmail.com>
2024-02-11 23:27:08 +0100

    working_navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css'
 import NavigationBar from './components/navigationBar'
-import Button from "@mui/material/Button" 
+
 
 
 
 function App() {
 
   return (
-    <section className='bg-red-100 w-screen h-screen flex flex-col justify-center items-center'>
+    <section className='bg-gray-100 w-screen h-screen flex flex-col justify-center items-center'>
       
-      <div className='bg-white w-[60%] mt-[10%] rounded-3xl shadow-lg'>
+      <div className='bg-white w-[75%] h-[10%] mt-[10%] rounded-3xl shadow-2xl drop-shadow-2xl'>
 
         <NavigationBar/>
-        <Button  variant="contained">Hello world</Button>;
+        
       </div>
 
     </section>
--- portfolio/src/components/navigationBar.jsx
+++ portfolio/src/components/navigationBar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import logo from "../assets/logo_dark.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const NavigationBar = () => {
   return (
 
-    <div className=" bg-white h-fit p-2 rounded-lg"> 
+    <div className=" bg-white h-fit p-2 rounded-lg py-3"> 
 
-       <aside>
-        <img className='w-[10%]' src={logo} alt='logo'/>
+       <aside className='flex items-center '>
+        <img className='w-[9%]' src={logo} alt='logo'/>
 
-        <span>
-          
-        </span>
+        <div className='text-black text-sm p-1 h-fit hover:bg-[#e45447] hover:text-white w-fit rounded-sm hover:shadow-lg hover:drop-shadow-md shadow-slate-900 hover:-rotate-3 duration-150  '>
+          <FontAwesomeIcon icon={faEnvelope}/>
+          <a className='text-black py-2 px-1 hover:text-white' href='mailto:driss.lakhzani.ds@gmail.com'>driss.lakhzani.ds@gmail.com</a>
+        </div>
        </aside>
 
     </div>
